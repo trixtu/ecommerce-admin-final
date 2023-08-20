a4562deef08be36b328f4a3e18e1964d6ea1a455
trixtu <133573198+trixtu@users.noreply.github.com>
2023-08-20 19:14:48 +0200

    final

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { toast } from "react-hot-toast"
 import { Trash } from "lucide-react"
-import { Category, Color, Image, Product, Size } from "@prisma/client"
+import { Category, Color, Image, Product, Size, Subcategory } from "@prisma/client"
 import { useParams, useRouter } from "next/navigation"
 
 import { Input } from "@/components/ui/input"
@@ -45,7 +45,7 @@ interface ProductFormProps {
   initialData: Product & {
     images: Image[]
   } | null;
-  categories: Category[];
+  categories: Subcategory[];
   colors: Color[];
   sizes: Size[];
 };
