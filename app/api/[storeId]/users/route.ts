9c69105f0f0a1719d33311ad02d80efb0f76b19f
trixtu <133573198+trixtu@users.noreply.github.com>
2023-08-22 06:17:00 +0200

    final

--- app/api/[storeId]/users/route.ts
+++ app/api/[storeId]/users/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-
 import prismadb from "@/lib/prismadb";
 
 // export async function POST(req: Request,) {
@@ -17,17 +16,24 @@ import prismadb from "@/lib/prismadb";
 //   }
 // };
 
-export async function GET(req: Request) {
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
   try {
-    const users = await prismadb.user.findMany({
-     where:{
-      id:'7cdbea0b-236e-460e-b5c6-f294af7bd53a'
-     }
-    });
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
 
+    const users = await prismadb.user.findMany({
+      where: {
+        storeId: params.storeId
+      }
+    });
+  
     return NextResponse.json(users);
   } catch (error) {
-    console.log("[USERS_GET]", error);
+    console.log('[USERS_GET]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
-}
+};
