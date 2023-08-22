1adcf6c417270d3fbb0447e5a37b86ed3c132144
trixtu <133573198+trixtu@users.noreply.github.com>
2023-08-22 08:11:59 +0200

    final

--- app/api/[storeId]/users/route.ts
+++ app/api/[storeId]/users/route.ts
@@ -1,6 +1,17 @@
 import { NextResponse } from "next/server";
 import prismadb from "@/lib/prismadb";
 
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
 export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
@@ -33,7 +44,7 @@ export async function POST(
       }
     });
   
-    return NextResponse.json(user);
+    return NextResponse.json({user},{headers:corsHeaders});
   } catch (error) {
     console.log('[USERS_POST]', error);
     return new NextResponse("Internal error", { status: 500 });
