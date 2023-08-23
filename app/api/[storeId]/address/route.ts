dfd7b44751a1c724205c60e0af6c326f7b9325d2
trixtu <133573198+trixtu@users.noreply.github.com>
2023-08-23 10:26:41 +0200

    final

--- app/api/[storeId]/address/route.ts
+++ app/api/[storeId]/address/route.ts
@@ -3,18 +3,15 @@ import prismadb from "@/lib/prismadb";
 import { headers } from "next/headers";
 import { hash } from "bcrypt";
 
-
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 export async function OPTIONS() {
   return NextResponse.json({}, { headers: corsHeaders });
 }
-
-
 
 export async function GET(
   req: Request,
@@ -34,6 +31,43 @@ export async function GET(
     return NextResponse.json(address);
   } catch (error) {
     console.log("[ADDRESS_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const {
+      data: { id, land, postzahl, stadt, street, telefon, firma },
+    } = await req.json();
+
+    const address = await prismadb.address.update({
+      where: {
+        id: id,
+      },
+      data: {
+        	land,
+          postzahl,
+          stadt,
+          street,
+          telefon,
+          firma
+      },
+    });
+
+    return NextResponse.json(
+      {
+        address: {
+          user: address.userId,
+        },
+      },
+      { headers: corsHeaders }
+    );
+  } catch (error) {
+    console.log("[ADDRESS_PATCH]", error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
