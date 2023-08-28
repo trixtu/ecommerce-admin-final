088f6c4be8763ae7872bc6d52d79c04a83bd5fe5
trixtu <trixtu12@gmail.com>
2023-08-28 18:58:32 +0200

    final

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,10 +1,6 @@
 import { redirect } from 'next/navigation';
 import { auth } from '@clerk/nextjs';
-
-import Navbar from '@/components/navbar'
 import prismadb from '@/lib/prismadb';
-
-import Sidebar from '@/components/sidebar';
 import Layout from '@/components/Layout';
 
 
